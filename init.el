0651794a563552ba442caf5f0e63f0eecf104fc9
3Gigs <14863137+3Gigs@users.noreply.github.com>
2022-01-06 17:15:36 -0800

    Set backup directory

--- init.el
+++ init.el
@@ -232,6 +232,13 @@
 (setq initial-buffer-choice (lambda () get-buffer-create "*dashboard*"))
 ; Disable auto-save
 (setq auto-save-default nil)
+;; Write backups to ~/.emacs.d/backup/
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying      t  ; Don't de-link hard links
+      version-control        t  ; Use version numbers on backups
+      delete-old-versions    t  ; Automatically delete excess backups:
+      kept-new-versions      20 ; how many of the newest versions to keep
+      kept-old-versions      5) ; and how many of the old
 
 ;; SECTION: SETTINGS FROM EMACS CUSTOMIZE
 (put 'upcase-region 'disabled nil)
