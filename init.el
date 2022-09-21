1e61a74995304a4c6055e08d7c1422c89afcd07c
Huaxuan Yang <yanghuaxuan@gmail.com>
2022-09-21 16:26:22 -0700

    Remove unnecessary additions from Custom

--- init.el
+++ init.el
@@ -408,28 +408,6 @@ require 'dap-cpptools))
 
 ; Set variables
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(menu-bar-mode nil)
  '(org-agenda-files nil nil nil "Customized with use-package org")
  '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:family "JetBrains Mono" :heigth 90))))
- '(org-block ((t (:weight normal :inherit (shadow fixed-pitch)))))
- '(org-document-info ((t (:inherit default :weight bold :foreground "#A0B3C5" :font "Lucida Grande" :height 1.3 :underline nil))))
- '(org-document-title ((t (:inherit default :weight bold :foreground "#A0B3C5" :font "Lucida Grande" :height 2.0 :underline nil))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "#A0B3C5" :font "Lucida Grande" :height 1.4))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "#A0B3C5" :font "Lucida Grande" :height 1.3))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "#A0B3C5" :font "Lucida Grande" :height 1.2))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "#A0B3C5" :font "Lucida Grande"))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#A0B3C5" :font "Lucida Grande"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#A0B3C5" :font "Lucida Grande"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#A0B3C5" :font "Lucida Grande"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#A0B3C5" :font "Lucida Grande"))))
- '(variable-pitch ((t (:font "Lucida Grande" :height 100 :weight thin)))))
